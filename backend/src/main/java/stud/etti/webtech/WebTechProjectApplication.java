7434f1cbac88376771ea7bf39f65aaff8696f301
Mihai Adrian <petrutm1975@gmail.com>
2025-11-30 23:17:43 +0200

    Add initial spring boot project

---
+++ backend/src/main/java/stud/etti/webtech/WebTechProjectApplication.java
@@ -0,0 +1,13 @@
+package stud.etti.webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTechProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTechProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/stud/etti/webtech/WebTechProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.etti.webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTechProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
